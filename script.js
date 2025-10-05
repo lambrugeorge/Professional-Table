4ee5423a6546824c056dd1bf5df7569e0e44e764
lambrugeorge <lambru_george@yahoo.com>
2025-10-05 22:49:36 +0300

    Fetch Users Javascript

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
+const spinner = document.getElementById('spinner');
+const table = document.getElementById('data-table');
+const tableBody = document.getElementById('table-body');
+const pagination = document.getElementById('pagination');
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+const pageNumber = document.getElementById('page-number');
+
+let data = [];
+
+// Fetch data from API
+
+async function fetchData() {
+    spinner.style.display = 'flex';
+    try {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        const response = await fetch('https://randomuser.me/api/?results=50');
+        const json = await response.json();
+        const data = json.results;
+        console.log(data);
+        displayTable(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    } finally {
+        spinner.style.display = 'none';
+        table.style.display = 'table';
+        pagination.style.display = 'block';
+    }
+}
 
 
+// Display table data
+function displayTable(dataToDisplay) {
+    tableBody.innerText = '';
+    dataToDisplay.forEach(user => {
+        const row =
+        `<tr>
+                <td data-label="Name">${user.name.first} ${user.name.last}</td>
+                <td data-label="Email">${user.email}</td>
+                <td data-label="Name">${user.login.username}</td>
+                <td data-label="Name">${user.location.country}</td>
+        </tr>`; 
+        tableBody.insertAdjacentHTML('beforeend', row);
+    });
+} 
 
 
-
-
+fetchData();
 // Dark Mode Functionality ---//
 const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
@@ -23,6 +65,7 @@ if (isDarkMode) {
 themeToggle.addEventListener('click', () => {
      body.style.transition = 'background-color 0.3s, color 0.3s';
     if (body.classList.contains('dark-mode')) {
+        body.classList.remove('dark-mode');
         themeToggle.innerHTML = 'Dark Mode';
         localStorage.setItem('dark-mode', 'false');
     } else {
